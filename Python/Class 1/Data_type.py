8690380c04c8fd2103b544b730630728470f35ed
maliksiddiq <maliksiddiqawan21@gmail.com>
2026-04-06 12:18:02 -0900

    First Class

---
+++ Python/Class 1/Data_type.py
@@ -0,0 +1,12 @@
+# first_num : int = 15
+# print(first_num) 
+# print(type(first_num)) 
+
+# sec_num : float = 15.5
+# print(sec_num) 
+# print(type(sec_num)) 
+
+
+# third_num : complex = 5 + 3j
+# print(third_num)
+# print(type(third_num))
