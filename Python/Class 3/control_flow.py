a01b52e25530c1d20e70e950c3f5fb33d6828668
maliksiddiq <maliksiddiqawan21@gmail.com>
2026-04-12 12:00:39 -0900

    Control Flow Done!

--- Python/Class 3/control_flow.py
+++ Python/Class 3/control_flow.py
@@ -117,3 +117,139 @@
 #     result = "Invalid operation."
 
 # print("Result:", result)
+
+# ------------------------------------------------------------------------------------
+
+# def check_status(code):
+#     match code:
+#         case  200:
+#             print("OK")
+#         case    400:
+#             print("Bad Request")     
+#         case   404:
+#             print("Not Found")
+#         case _:
+#             print("Unknown Status")
+
+
+# check_status(200)    
+# check_status(400)    
+# check_status(404)    
+# check_status('_')    
+
+
+# ------------------------------------------------------------------------------------
+
+# fruits : list = ['Apple' , 'Banana' , 'Orange' , 'Strawbeery']
+
+# for fruit in fruits:
+#     print(fruits)
+
+# word: str = "Python"
+# for letter in word:
+#     print(letter)
+
+
+# --------------------------------------------- Loops Wih else Block ----------------
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for num in numbers:
+#     print(num)
+# else:
+#     print("Loop completed successfully!")
+
+
+# ------------------------------------------ break ---------------
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for num in numbers:
+#     print(num)
+#     if num == 3:
+#         print("Breaking the loop!")
+#         break
+# else:
+#     print("Loop completed successfully!")  # This will NOT run
+
+# -------------------------------------------------
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for num in numbers:
+#     if num == 6:
+#         print("Number found!")
+#         break
+# else:
+#     print("Number not found!")  # Runs because 6 is not in the list
+
+
+#  -------------------------------------------------
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(2 , 11 , 2):
+#     print(i)
+
+#  ! -------------------------------------------------throway Variable _ temporary variable --------------------
+
+# !  _ “Mujhe ye value mil rahi hai, lekin mujhe iski zarurat nahi hai.”
+
+# for _ in range(5):
+#     print(f'Hello')
+
+
+# for i in range(5):
+#     print(i , 'Hello World!')
+    
+
+# for _ in range(10):
+#     print(f"Hello, World! Iteration { _ }")
+
+
+# ! ------------------------------------ While Loop -----------------------------
+
+# count: int = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+
+# ! ------------------------------------------------ Break and Coninue ---------------------
+
+# for i in range(10):
+#     if i == 5:  
+#          break     
+#     print(i)  
+
+
+# for i in range(5):
+#     if i == 3:
+#         continue
+#     print(i)  # Prints 0, 1, 2, 4
+
+
+# ! ----------------------------------------- Nested Loops ! ------------------------
+
+# for outer in range(1 , 6):
+#     print(f'Muliplication table for {outer} :')
+#     for inner in range(1 , 6):
+#         print(f'{outer} * {inner} = {outer * inner}')
+#     print()
+
+
+# -----------------------------------------------------------
+
+# total: int = 0
+# for i in range(1, 101):
+#     total += i
+# print("Sum of numbers from 1 to 100:", total)    
+
+
+# num: int = 24
+# factors = []
+# for i in range(1, num + 1):
+#     if num % i == 0:
+#         factors.append(i)
+# print(f"Factors of {num}: {factors}")
